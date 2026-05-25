ade1723dfd25d967caf73e6245a207dff18517cf
leo-morikio <leo.morikio@gmail.com>
2026-05-25 19:42:24 -0300

    Commit Inicial

---
+++ src/test/java/br/ufscar/dc/dsw/pescd/PescdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.pescd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PescdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
